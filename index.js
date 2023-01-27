d7227c206b105ac94bb063dfeada55bd3bbb806a
max mcruvie <mcruvie@hotmail.co.uk>
2023-01-27 07:52:45 +0000

    Path not required

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Require express
 const express = require('express');
 const app = express();
-var path = require('path')
 
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/public'));
