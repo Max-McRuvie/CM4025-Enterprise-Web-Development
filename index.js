8df511d42fef6a1033b6c9cf035f07665c391ec6
max mcruvie <mcruvie@hotmail.co.uk>
2023-02-02 11:54:27 +0000

    price calculation in backend

--- index.js
+++ index.js
@@ -12,4 +12,17 @@ app.get('/', function(req, res) {
     res.render('myWebPage.html');
 })
 
+app.get('/api/getPrice', function(req, res) {
+    var s = req.query.salary;
+    var d = req.query.days;
+    console.log("Calculating price")
+    console.log(s)
+    console.log(d)
+    dailyRate = s/365;
+    price = Math.round(dailyRate * d);
+    var roundToNearest = 50;
+    roundedPrice = Math.round((price+roundToNearest)/roundToNearest) * roundToNearest // Always round up
+    res.send(""+roundedPrice)
+})
+
 app.listen(3000);
